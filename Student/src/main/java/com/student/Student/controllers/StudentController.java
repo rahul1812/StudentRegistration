be8bc653cac1daff7ea141985529c12f1b752897
Rahul Srivastasva <rahul.srivastava@rsystems.com>
2023-07-12 17:41:26 +0530

    endpoint for delete and update added

--- Student/src/main/java/com/student/Student/controllers/StudentController.java
+++ Student/src/main/java/com/student/Student/controllers/StudentController.java
@@ -1,15 +1,11 @@
 package com.student.Student.controllers;
 
-
-
 import com.student.Student.dao.StudentDao;
 import com.student.Student.dao.StudentDaoImpl;
 import com.student.Student.entites.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,17 +14,36 @@ import java.util.Optional;
 public class StudentController {
     @Autowired
    private StudentDao studentDao;
-    @GetMapping("/")
+    @GetMapping("/getAll")
     public ResponseEntity<List<Student>> getAllStudent() {
         List<Student> studentList = studentDao.list();
         return ResponseEntity.ok(studentList);
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/getById/{id}")
     public ResponseEntity<Student> getStudentById(@PathVariable int id) {
         Optional<Student> students = studentDao.get(id);
         return ResponseEntity.ok(students.get());
     }
+     @PostMapping("/save")
+    public ResponseEntity getStudentById(@RequestBody Student student) {
+         Optional<Integer> result = studentDao.create(student);
+         return  (result.get()==1)?ResponseEntity.ok("Data added Successfully"):
+                 ResponseEntity.badRequest().body("Failed to delete student");
+    }
 
+    @PutMapping("/update/{id}")
+    public ResponseEntity getStudentById(@RequestBody Student student,@PathVariable int student_id) {
+        Optional<Integer> result = studentDao.update(student,student_id);
+        return  (result.get()==1)?ResponseEntity.ok("Data Updated Successfully"):
+                ResponseEntity.badRequest().body("Failed to delete student");
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteStudent(@PathVariable int student_id){
+        Optional<Integer> result = studentDao.delete(student_id);
+       return  (result.get()==1)?ResponseEntity.ok("Student deleted successfully"):
+                ResponseEntity.badRequest().body("Failed to delete student");
+    }
 
 }
