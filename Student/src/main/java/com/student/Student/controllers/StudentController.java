23907d847f666d04dceb7849f649564541fdaab9
Rahul Srivastasva <rahul.srivastava@rsystems.com>
2023-07-12 14:32:21 +0530

    new endpoint added in contoller

--- Student/src/main/java/com/student/Student/controllers/StudentController.java
+++ Student/src/main/java/com/student/Student/controllers/StudentController.java
@@ -6,10 +6,13 @@ import com.student.Student.dao.StudentDao;
 import com.student.Student.dao.StudentDaoImpl;
 import com.student.Student.entites.Student;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class StudentController {
@@ -18,5 +21,11 @@ public class StudentController {
     @GetMapping("/getAll")
     public List<Student> getAllStudent() {
         return studentDao.list();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Student> getStudentById(@PathVariable int id) {
+        Optional<Student> students = studentDao.get(id);
+        return ResponseEntity.ok(students.get());
     }
 }
